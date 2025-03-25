68263dd80a25261d412844e6ef2673fa237fdc47
sourav <raavisourav@gmail.com>
2025-03-25 16:48:21 +0530

    added scripts

--- task05/outputs.tf
+++ task05/outputs.tf
@@ -1,4 +1,4 @@
-output "traffic_manager_profile" {
-  value       = module.traffic_manager.profile_id
+output "traffic_manager_fqdn" {
+  value       = module.traffic_manager.fqdn
   description = "value"
 }
